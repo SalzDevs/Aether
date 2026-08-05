7ac656460d9640f344bc8d7f2a2d73229362a393
SalzDevs <a49000@alunos.isel.pt>
2026-08-05 23:40:20 +0100

    Inintial scafold of the Qeue structure that will hold sensor data

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "scheduler/scheduler.h"
 #include "sensor/sensor.h"
 
+typedef struct {
+  size_t intial_size;
+  size_t current_size;
+  sensor* data;
+} sensorQeue;
+
+
+void initSensorQeue(sensorQeue* sq) {
+  sq->intial_size = 100;
+  sq->current_size = 0;
+  sq->data = (sensor*)malloc(sizeof(sensor)); 
+}
+
 
 int main() {
+  sensorQeue sq;
+  initSensorQeue(&sq);
+  printf("sensorQeue data after init: {%zu} {%zu} {%p}\n",sq.intial_size, sq.current_size, sq.data);
   sensor s;
   initSensor(&s);
   printSensorData(s);
