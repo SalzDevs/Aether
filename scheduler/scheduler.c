f1868bcdd5f1222a09016a1266a6815cfe18e5c6
SalzDevs <a49000@alunos.isel.pt>
2026-08-09 16:20:08 +0100

    make task be able to run functions at x periodicity

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -2,17 +2,25 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void initTask(Task* t, uint32_t period) {
+void initTask(Task* t, uint32_t period,void (*function)(void)) {
   time_t currentTime;
   time(&currentTime);
 
   t->period = period;
-  t->last_run = currentTime; 
+  t->last_run = currentTime;
+  t->function = function;
 }
 
 void printTask(Task task) {
   printf("Task Period:%u\n", task.period);
   printf("Task Last Run:%llu\n", task.last_run);
+}
+
+void runTask(Task* t) {
+  if (shouldRunTask(t->period, t->last_run)) {
+    t->function();
+    t->last_run = time(NULL);
+  }
 }
 
 bool shouldRunTask(uint32_t period, uint64_t last_run) {
--- scheduler/scheduler.h
+++ scheduler/scheduler.h
@@ -5,21 +5,25 @@
 #include <stdbool.h>
 #include <time.h>
 
-//TODO: add to the struct the void function pointer to have the function that the Task should execute
 typedef struct {
   //period of running the task
   uint32_t period;
   // time of the last run
   uint64_t last_run;
+  // executable task function
+  void (*function)(void);
 } Task;
 
 // Initiate the Task Strucure
-void initTask(Task* t, uint32_t period);
+void initTask(Task* t, uint32_t period,  void (*function)(void));
 
 //Prints to standard output task values
 void printTask(Task task);
 
 //Evaluates if enough time as passed to run Task 
 bool shouldRunTask(uint32_t period, uint64_t last_run);
+
+// Runs the task if the period as passed
+void runTask(Task* t); 
 
 #endif
