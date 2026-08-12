ad4b91ae6d93379f24dd75533c65b6af62c44946
SalzDevs <a49000@alunos.isel.pt>
2026-08-12 23:23:58 +0100

    create UI display horizontal blocks to show sensor data

--- main.c
+++ main.c
@@ -18,6 +18,10 @@ static void mockSensorTask(void *ctx) {
   sensorDataUpdate(&args->sq->data[args->sensorIdx]);
 }
 
+static int displayBlockHeightCalc(int screenHeight, int amountOfSensors) {
+  return screenHeight/amountOfSensors;
+} 
+
 int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 450;
@@ -35,14 +39,17 @@ int main(void) {
   sensor s1;
   sensor s2;
   sensor s3;
+  sensor s4;
 
   initSensor(&s1);
   initSensor(&s2);
   initSensor(&s3);
+  initSensor(&s4);
 
   addElemToQueue(&sq, s1);
   addElemToQueue(&sq, s2);
   addElemToQueue(&sq, s3);
+  addElemToQueue(&sq, s4);
 
   taskArgs args1 = {
     .sq = &sq,
@@ -59,19 +66,31 @@ int main(void) {
     .sensorIdx = 2
   };
 
+  
+  taskArgs args4 = {
+    .sq = &sq,
+    .sensorIdx = 2
+  };
+
   // Create tasks
   Task sensorTask1;
   Task sensorTask2;
   Task sensorTask3;
+  Task sensorTask4;
 
   initTask(&sensorTask1, 3, mockSensorTask, &args1,time(NULL));
   initTask(&sensorTask2, 3, mockSensorTask, &args2,time(NULL));
   initTask(&sensorTask3, 3, mockSensorTask, &args3,time(NULL));
+  initTask(&sensorTask4, 3, mockSensorTask, &args4,time(NULL));
+  
+  int displayBlockHeight = displayBlockHeightCalc(screenHeight,sq.current_size);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Hello", 400, 150, 20, RED);
+    for (size_t i = 0; i < sq.current_size; i++) {
+      DrawLine(0, displayBlockHeight*i, screenWidth, displayBlockHeight*i, RED);
+    }
     EndDrawing();
     runTask(&sensorTask1);
     runTask(&sensorTask2);
