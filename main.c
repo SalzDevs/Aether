581bb5f22b445f46ac2e4d7dcd221468ba45a48a
SalzDevs <a49000@alunos.isel.pt>
2026-08-14 12:10:29 +0100

    display elapsed time since app start in the UI

--- main.c
+++ main.c
@@ -85,16 +85,16 @@ int main(void) {
   
   int displayBlockHeight = displayBlockHeightCalc(screenHeight,sq.current_size);
   char buf[128];
-
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    
+    DrawText(TextFormat("Time: %.02f", GetTime()), 0, 0, 10, BLACK); 
     for (size_t i = 0; i < sq.current_size; i++) {
       sensorToString(sq.data[i], buf, sizeof(buf));
       DrawText(buf, screenWidth/2-300, (displayBlockHeight*i)+(displayBlockHeight/2), 15, BLUE);
       DrawLine(0, displayBlockHeight*i, screenWidth, displayBlockHeight*i, RED);
     }
+
     EndDrawing();
     runTask(&sensorTask1);
     runTask(&sensorTask2);
@@ -102,6 +102,5 @@ int main(void) {
     runTask(&sensorTask4);
     printQeue(&sq);
   }
-
   return 0;
 }
