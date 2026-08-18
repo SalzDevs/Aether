815db8b2a7f7fb6ad8c7bffa237abdebb21465de
SalzDevs <145263589+SalzDevs@users.noreply.github.com>
2026-08-18 17:05:34 +0100

    Update sensor count and assertions in test_config.c
    Updated the expected sensor count and added assertions for the new sensor data.

--- tests/test_config.c
+++ tests/test_config.c
@@ -8,15 +8,24 @@ static void test_file_to_sensors(void) {
   sensorData* sensors = fileToSensors("config/sensors.yaml", &count);
 
   assert(sensors != NULL);
-  assert(count == 4);
+  assert(count == 5);
+
   assert(sensors[0].sensorId == 1);
   assert(sensors[0].airSpeed == 120.5f);
   assert(sensors[0].altitude == 1000.0f);
   assert(sensors[0].engineTemperature == 85);
   assert(sensors[0].fuelLevel == 60);
   assert(sensors[0].baterryVoltage == 12);
+
   assert(sensors[3].sensorId == 4);
   assert(sensors[3].fuelLevel == 42);
+
+  assert(sensors[4].sensorId == 5);
+  assert(sensors[4].airSpeed == 115.8f);
+  assert(sensors[4].altitude == 1100.0f);
+  assert(sensors[4].engineTemperature == 87);
+  assert(sensors[4].fuelLevel == 42);
+  assert(sensors[4].baterryVoltage == 12);
 
   free(sensors);
 }
@@ -24,6 +33,7 @@ static void test_file_to_sensors(void) {
 static void test_file_to_sensors_missing_file(void) {
   size_t count = 0;
   sensorData* sensors = fileToSensors("config/does_not_exist.yaml", &count);
+
   assert(sensors == NULL);
   assert(count == 0);
 }
